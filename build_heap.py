2e03e6388137a6a28f78399fb2b4faa3bbf777da
HelenaAnnaBunka <125254498+HelenaAnna@users.noreply.github.com>
2023-03-19 22:59:50 +0000

    ?

--- build_heap.py
+++ build_heap.py
@@ -6,10 +6,11 @@ def build_heap(data):
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
     for i in range(1, len(data)):
-        while i > 0 and data[(i -1 ) // 2] > data[i]:
-            data[i], data[(i - 1) // 2] = data[(i - 1) // 2], data[i]
-            swaps.append(((i - 1) // 2, i))
-            i = (i - 1) // 2
+        j = i
+        while j > 0 and data[(j - 1) // 2] > data[j]:
+            swaps.append(((j - 1) // 2, j))
+            data[(j - 1) // 2], data[j] = data[j], data[(j - 1) // 2]
+            j = (j - 1) // 2
 
     return swaps
 
