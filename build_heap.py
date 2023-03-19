cf5413add3eecb4ad736a21ee5c1eee1b359df7a
HelenaAnnaBunka <125254498+HelenaAnna@users.noreply.github.com>
2023-03-19 22:34:19 +0000

    ?

--- build_heap.py
+++ build_heap.py
@@ -9,7 +9,7 @@ def build_heap(data):
     for i in range(n // 2, -1, -1):
         swaps = uz_leju(data, i, n, swaps)
 
-    for i in range (n-1, 0, -1):
+    for i in range (n - 1, 0, -1):
         data[0], data[i] = data[i], data[0]
         swaps.append((0, i))
         swaps = uz_leju(data, 0, i, swaps)
